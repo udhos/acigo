f019b83b57ac9ebba838f08b718db4fee631c591
Everton Marques <everton.marques@gmail.com>
2017-01-19 14:29:06 -0200

    Bridge domains.

---
+++ aci/bd.go
@@ -0,0 +1,95 @@
+package aci
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func rnBridgeDomain(bd string) string {
+	return "BD-" + bd
+}
+
+func dnBridgeDomain(tenant, bd string) string {
+	return rnTenant(tenant) + "/" + rnBridgeDomain(bd)
+}
+
+// BridgeDomainAdd creates a new bridge domain in a tenant.
+func (c *Client) BridgeDomainAdd(tenant, bd, descr string) error {
+
+	me := "BridgeDomainAdd"
+
+	rn := rnBridgeDomain(bd)
+
+	dn := dnBridgeDomain(tenant, bd)
+
+	api := "/api/node/mo/uni/" + dn + ".json"
+
+	url := c.getURL(api)
+
+	j := fmt.Sprintf(`{"fvBD":{"attributes":{"dn":"uni/%s","name":"%s","descr":"%s","rn":"%s","status":"created"}}}`,
+		dn, bd, descr, rn)
+
+	c.debugf("%s: url=%s json=%s", me, url, j)
+
+	body, errPost := c.post(url, contentTypeJSON, bytes.NewBufferString(j))
+	if errPost != nil {
+		return fmt.Errorf("%s: %v", me, errPost)
+	}
+
+	c.debugf("%s: reply: %s", me, string(body))
+
+	return parseJSONError(body)
+}
+
+// BridgeDomainDel deletes an existing bridge domain from a tenant.
+func (c *Client) BridgeDomainDel(tenant, bd string) error {
+
+	me := "BridgeDomainDel"
+
+	rnT := rnTenant(tenant)
+
+	dn := dnBridgeDomain(tenant, bd)
+
+	api := "/api/node/mo/uni/" + rnT + ".json"
+
+	url := c.getURL(api)
+
+	j := fmt.Sprintf(`{"fvTenant":{"attributes":{"dn":"uni/%s","status":"modified"},"children":[{"fvBD":{"attributes":{"dn":"uni/%s","status":"deleted"}}}]}}`,
+		rnT, dn)
+
+	c.debugf("%s: url=%s json=%s", me, url, j)
+
+	body, errPost := c.post(url, contentTypeJSON, bytes.NewBufferString(j))
+	if errPost != nil {
+		return fmt.Errorf("%s: %v", me, errPost)
+	}
+
+	c.debugf("%s: reply: %s", me, string(body))
+
+	return parseJSONError(body)
+}
+
+// BridgeDomainList retrieves the list of bridge domains from a tenant.
+func (c *Client) BridgeDomainList(tenant string) ([]map[string]interface{}, error) {
+
+	me := "BridgeDomainList"
+
+	key := "fvBD"
+
+	t := rnTenant(tenant)
+
+	api := "/api/node/mo/uni/" + t + ".json?query-target=children&target-subtree-class=" + key
+
+	url := c.getURL(api)
+
+	c.debugf("%s: url=%s", me, url)
+
+	body, errGet := c.get(url)
+	if errGet != nil {
+		return nil, fmt.Errorf("%s: %v", me, errGet)
+	}
+
+	c.debugf("%s: reply: %s", me, string(body))
+
+	return jsonImdataAttributes(c, body, key, me)
+}
---
+++ samples/aci-bd/bd.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/udhos/acigo/aci"
+)
+
+func main() {
+
+	debug := os.Getenv("DEBUG") != ""
+
+	if len(os.Args) < 3 {
+		log.Fatalf("usage: %s add|del|list args", os.Args[0])
+	}
+
+	a, errLogin := login(debug)
+	if errLogin != nil {
+		log.Printf("exiting: %v", errLogin)
+		return
+	}
+
+	defer logout(a)
+
+	execute(a, os.Args[1], os.Args[2:])
+
+	// display existing
+
+	tenant := os.Args[2]
+
+	list, errList := a.BridgeDomainList(tenant)
+	if errList != nil {
+		log.Printf("could not list bridge domains: %v", errList)
+		return
+	}
+
+	for _, t := range list {
+		name := t["name"]
+		dn := t["dn"]
+		mac := t["mac"]
+		descr := t["descr"]
+		log.Printf("found bridge domain: name=%s dn=%s mac=%s descr=%s\n", name, dn, mac, descr)
+	}
+}
+
+func execute(a *aci.Client, cmd string, args []string) {
+	switch cmd {
+	case "add":
+		if len(args) < 2 {
+			log.Fatalf("usage: %s add tenant bridge-domain [descr]", os.Args[0])
+		}
+		tenant := args[0]
+		bd := args[1]
+		var descr string
+		if len(args) > 2 {
+			descr = args[2]
+		}
+		errAdd := a.BridgeDomainAdd(tenant, bd, descr)
+		if errAdd != nil {
+			log.Printf("FAILURE: add error: %v", errAdd)
+			return
+		}
+		log.Printf("SUCCESS: add: %s %s", tenant, bd)
+	case "del":
+		if len(args) < 2 {
+			log.Fatalf("usage: %s del tenant bridge-domain", os.Args[0])
+		}
+		tenant := args[0]
+		bd := args[1]
+		errDel := a.BridgeDomainDel(tenant, bd)
+		if errDel != nil {
+			log.Printf("FAILURE: del error: %v", errDel)
+			return
+		}
+		log.Printf("SUCCESS: del: %s %s", tenant, bd)
+	case "list":
+	default:
+		log.Printf("unknown command: %s", cmd)
+	}
+}
+
+func login(debug bool) (*aci.Client, error) {
+
+	a, errNew := aci.New(aci.ClientOptions{Debug: debug})
+	if errNew != nil {
+		return nil, fmt.Errorf("login new client error: %v", errNew)
+	}
+
+	errLogin := a.Login()
+	if errLogin != nil {
+		return nil, fmt.Errorf("login error: %v", errLogin)
+	}
+
+	return a, nil
+}
+
+func logout(a *aci.Client) {
+	errLogout := a.Logout()
+	if errLogout != nil {
+		log.Printf("logout error: %v", errLogout)
+		return
+	}
+}
