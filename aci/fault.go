dfa7a96443a536b4c3a74c6ae85929b120eed014
Joshua M. Dotson <josdotso@cisco.com>
2019-07-02 15:13:58 +0000

    Add a.FaultList()

---
+++ aci/fault.go
@@ -0,0 +1,27 @@
+package aci
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// FaultList retrieves the list of faults in the fabric.
+func (c *Client) FaultList() ([]map[string]interface{}, error) {
+
+	key := "faultInst"
+
+	api := "/api/node/class/" + key + ".json"
+
+	url := c.getURL(api)
+
+	c.debugf("FaultList: url=%s", url)
+
+	body, errGet := c.get(url)
+	if errGet != nil {
+		return nil, errGet
+	}
+
+	c.debugf("FaultList: reply: %s", string(body))
+
+	return jsonImdataAttributes(c, body, key, "FaultList")
+}
